a3e8a1357c4b34c81eb662a2dece19c42fc7d8e5
meiki <mehdinourollah@gmail.com>
2020-11-25 20:32:18 +0300

    Updates

---
+++ precache-manifest.dff00d662ef7417a01e181751963cdca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cdbc444bde8a49ddb261ee2c1c37251",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fe4ae077e6b8acb8c79",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "c372e812b9eebd63a6ac",
+    "url": "/static/css/main.5caccf67.chunk.css"
+  },
+  {
+    "revision": "5fe4ae077e6b8acb8c79",
+    "url": "/static/js/2.b43ae1b2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.b43ae1b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c372e812b9eebd63a6ac",
+    "url": "/static/js/main.6f9c888c.chunk.js"
+  },
+  {
+    "revision": "4b041d7b71a4e598f53b",
+    "url": "/static/js/runtime-main.dbb89ece.js"
+  }
+]);
