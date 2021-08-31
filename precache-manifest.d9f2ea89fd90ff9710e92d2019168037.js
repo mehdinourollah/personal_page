eced59428af6ea457f5f8ab8e0598bd98c45a0e7
Mehdi Nourollah <mehdi.nourollah@tegsoft.com>
2021-08-31 11:59:19 +0300

    Updates

---
+++ precache-manifest.d9f2ea89fd90ff9710e92d2019168037.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52489e303abe99f674e12a1f9a813660",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fe4ae077e6b8acb8c79",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "4c9d7fe36ca827980583",
+    "url": "/static/css/main.6aecdc20.chunk.css"
+  },
+  {
+    "revision": "5fe4ae077e6b8acb8c79",
+    "url": "/static/js/2.b43ae1b2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.b43ae1b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c9d7fe36ca827980583",
+    "url": "/static/js/main.24262263.chunk.js"
+  },
+  {
+    "revision": "4b041d7b71a4e598f53b",
+    "url": "/static/js/runtime-main.dbb89ece.js"
+  }
+]);
